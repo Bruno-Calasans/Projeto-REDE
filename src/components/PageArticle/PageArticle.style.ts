ce2783658e38e09ba6949c1fc6372e538b3d2b30
Bruno Calasans <83395524+Bruno-Calasans@users.noreply.github.com>
2023-05-14 15:57:41 -0300

    feat: favicon e título da página

--- src/components/PageArticle/PageArticle.style.ts
+++ src/components/PageArticle/PageArticle.style.ts
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 export const Container = styled.section`
   display: flex;
   flex-direction: column;
+  width: 100%;
   height: 500px;
 
   background-color: beige;
